8f6f2f3df0f1db4c42bbfd6eda21728dd3c3fd35
Siddharth Dange <siddharthdange@example.com>
2026-02-15 23:32:55 +0530

    Update main.py to pass MemoryStore to agents and use queue-based EventBus

--- src/main.py
+++ src/main.py
@@ -2,17 +2,21 @@ from src.orchestration.event_bus import EventBus
 from src.agents.ingestion_agent.agent import IngestionAgent
 from src.agents.profiling_agent.agent import ProfilingAgent
 from src.data.schemas import Event
+from src.core.memory_store import MemoryStore
 
 def main():
     event_bus = EventBus()
+    memory =  MemoryStore()
 
     ingestionAgent = IngestionAgent(
         name="IngestionAgent",
-        event_bus=event_bus)
+        event_bus=event_bus,
+        memory=memory)
     
     profiling_agent = ProfilingAgent(
         name = "ProfilingAgent",
-        event_bus=event_bus
+        event_bus=event_bus,
+        memory=memory
     )
 
     #Register Agent
@@ -29,7 +33,9 @@ def main():
         },
         source_agent="system"
     )
+    # change it to queue based structure
     event_bus.publish(initial_event)
+    event_bus.start()   # added this function
 
 if __name__ == "__main__":
     main()
