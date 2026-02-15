0f8699ad57586d0cb9665f5c42869d461da10718
Siddharth Dange <siddharthdange@example.com>
2026-02-15 16:13:57 +0530

    Added orchestration folder for agentic ai system

---
+++ src/orchestration/event_bus.py
@@ -0,0 +1,33 @@
+from typing import Dict, Any, List
+import threading
+
+class EventBus:
+    """
+    Simple in memory event bus.
+
+    Responsibilities:
+    - Register agents
+    - Publish events
+    - notify Interested agents 
+    """
+
+    def __init__(self):
+        self.subscribers: List[Any] = []   # Stores all agents registered to system.
+        self.lock = threading.Lock()
+        
+
+    def register(self,agent):
+        """
+        Register an agent to listen for events.
+        """
+        with self.lock:
+            self.subscribers.append(agent)
+    
+    def publish(self, event: Dict[str,Any]):
+        """
+        Publish an event to all interested agents.
+        """
+        with self.lock:
+            for agent in self.subscribers:
+                if agent.can_handle(event['event_type']):
+                    agent.handle(event)
