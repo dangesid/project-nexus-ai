9c719bd34fb716dbf75372a439e302a26a54207d
Siddharth Dange <siddharthdange@example.com>
2026-02-15 23:32:09 +0530

    Integrate MemoryStore in IngestionAgent to save processed user activity

--- src/agents/ingestion_agent/agent.py
+++ src/agents/ingestion_agent/agent.py
@@ -5,7 +5,16 @@ from src.data.schemas import Event
 class IngestionAgent(BaseAgent):
     """
     Handles Raw data ingestion
+
+    Responsibilities:
+    - Process incoming raw data events
+    - Save processed data to MemoryStore
+    - Publish new events to the EventBus
     """
+    
+    def __init__(self, name, event_bus, memory):
+        super().__init__(name, event_bus,memory)
+
 
     def can_handle(self, event: str) -> bool:
         return event.event_type == "raw_data_received"
@@ -20,6 +29,12 @@ class IngestionAgent(BaseAgent):
             "item_id": raw_payload.get("item_id"),
             "action": raw_payload.get("action"),
         }
+        # save processed data to memory
+        memory_key = f"user_activit:{processing_data['user_id']}"
+        self.memory.save(memory_key, processing_data)
+        print(f"[{self.name}] Saved process data to memoryStore under key; {memory_key}")
+
+
         print(f"[{self.name}] Publishing processed data event")
 
         self.publish(
